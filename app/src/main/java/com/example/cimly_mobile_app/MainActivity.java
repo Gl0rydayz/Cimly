f16f5a1645879ec5d9f30e81e695e5a98afa8052
Oussama IOUIRY <oussamaiouiry@gmail.com>
2023-12-05 13:40:18 +0100

    modify home screen's layout

--- app/src/main/java/com/example/cimly_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/cimly_mobile_app/MainActivity.java
@@ -1,11 +1,27 @@
 package com.example.cimly_mobile_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_enter, btn_exit, btn_display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +31,26 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_enter = findViewById(R.id.btn_enter);
+        btn_exit = findViewById(R.id.btn_exit);
+        btn_display = findViewById(R.id.btn_display);
+
+        // Change status bar and navigation bar color and items color
+        changeStatusBarColorAndTextColor(getResources().getColor(R.color.status_nav_bar_color));
     }
+
+    //region Method responsible for changing bars color
+    private void changeStatusBarColorAndTextColor(int color) {
+        Window window = getWindow();
+
+        // Set color for status bar
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(color);
+
+        // Set text color to dark
+        View decor = window.getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+    //endregion
 }
