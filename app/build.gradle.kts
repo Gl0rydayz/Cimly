4b83e306ba23706a715226bc044bf771130e9e29
Oussama IOUIRY <oussamaiouiry@gmail.com>
2023-12-05 12:09:50 +0100

    create home screen's layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.cimly_mobile_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cimly_mobile_app"
--- app/src/main/java/com/example/cimly_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/cimly_mobile_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cimly_mobile_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -8,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Disable Dark mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
