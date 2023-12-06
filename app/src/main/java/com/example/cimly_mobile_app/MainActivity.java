143b0e2da18f4a4b44cae17e7bbb021347400d83
Yasser <yassermechhab007@gmail.com>
2023-12-06 16:01:18 +0100

    set some changes in layout & handle landscape

--- app/src/main/java/com/example/cimly_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/cimly_mobile_app/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.cimly_mobile_app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Space;
 
 public class MainActivity extends AppCompatActivity {
+
+    Space spacer01 ;
+    Space spacer02 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Check if the orientation has changed
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_land);
+
+
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.activity_main);
+
+        }
+    }
+
+
 }
